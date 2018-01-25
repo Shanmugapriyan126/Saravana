011bc408be389ad5becb7da980c0f1d5cc6b0a75
Shanmugapriyan126 <35727800+Shanmugapriyan126@users.noreply.github.com>
2018-01-25 16:24:34 +0530

    Create even r odd.C

---
+++ even r odd.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the number A:");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("Number is even");
+    }
+    else
+    {
+        printf("Number is odd");
+    }
+}
