280bfa6d6430d32916ec3f08ae70231971a790df
Shanmugapriyan126 <35727800+Shanmugapriyan126@users.noreply.github.com>
2018-01-25 21:01:36 +0530

    Create Character or number.c

---
+++ Character or number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    char N;
+    printf("Enter the variable N:");
+    scanf("%c",&N);
+    if(N=='A'||N=='B'||N=='C'||N=='D'||N=='E'||N=='F'||N=='G'||N=='H'||N=='I'||N=='J'||N=='K'||N=='L'||N=='M'||N=='N'||N=='O'||N=='P'||N=='Q'||N=='R'||N=='S'||N=='T'||N=='U'||N=='W'||N=='X'||N=='Y'||N=='Z'||N=='a'||N=='b'||N=='c'||N=='d'||N=='e'||N=='f'||N=='g'||N=='h'||N=='i'||N=='j'||N=='k'||N=='l'||N=='m'||N=='n'||N=='o'||N=='p'||N=='q'||N=='r'||N=='s'||N=='t'||N=='u'||N=='v'||N=='w'||N=='x'||N=='y'||N=='z')
+    {
+        printf("It is a character");
+    }
+    else
+    {
+        printf("It is a number");
+    }
+}
