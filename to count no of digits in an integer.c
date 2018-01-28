bd92e0eec10ec64de15dd4d2004913bde4a72a7f
Shanmugapriyan126 <35727800+Shanmugapriyan126@users.noreply.github.com>
2018-01-28 22:19:23 +0530

    Create to count no of digits in an integer.c

---
+++ to count no of digits in an integer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    long long n;
+    int a = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    while(n!=0)
+    {
+        n /= 10;
+        ++a;
+    }
+
+    printf("Number of digits: %d", a);
+}
