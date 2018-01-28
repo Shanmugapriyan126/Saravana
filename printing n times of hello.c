757c073d255a8077cdde7c8db6dbab930a16dcee
Shanmugapriyan126 <35727800+Shanmugapriyan126@users.noreply.github.com>
2018-01-28 21:52:09 +0530

    Create printing n times of hello.c

---
+++ printing n times of hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the value for N:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("Hello");
+        printf("\n");
+    }
+}
