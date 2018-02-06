2f146fa252762c00ba46f3d67881cdd2fc6cb1e1
Shanmugapriyan126 <35727800+Shanmugapriyan126@users.noreply.github.com>
2018-02-06 16:18:30 +0530

    Create Odd number between two limits.c

---
+++ Odd number between two limits.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int x,y;
+    printf("Enter the two numbers:");
+    scanf("%d%d",&x,&y);
+    for(int i=x+1;i<y;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("\t%d",i);
+        }
+    }
+}
