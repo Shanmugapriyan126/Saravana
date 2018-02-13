d90c7f9a039f2da9632587e7f2fac993fc9de3c3
Shanmugapriyan126 <35727800+Shanmugapriyan126@users.noreply.github.com>
2018-02-13 10:50:47 +0000

    Create Arrayoflargernumber.c

---
+++ Arrayoflargernumber.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int N[10],n,a,i;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    printf("Enter the array elements:");
+    scanf("%d",&N[n]);
+    for(int i=1;i<n;i++)
+    {
+        scanf("%d",&N[i]);
+    }
+    for(int i=1;i<n;i++)
+    {
+            if(N[0]<N[i])
+            {
+                N[0]=N[i];
+            }
+    }
+    printf("\n %d is greater",N[i]);
+}
