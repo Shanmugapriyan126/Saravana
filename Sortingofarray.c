9aaa5b73d4032710fffb3ed19ab045f0b68610be
Shanmugapriyan126 <35727800+Shanmugapriyan126@users.noreply.github.com>
2018-02-13 11:19:59 +0000

    Create Sortingofarray.c

---
+++ Sortingofarray.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    int N[10],n,a,i,j;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    printf("Enter the array elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&N[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(N[i]>N[j])
+            {
+                a=N[i];
+                N[i]=N[j];
+                N[j]=a;
+            }
+        }
+    }
+    printf("\n Sorted Array Elements:");
+    for(int i=0;i<n;i++)
+    {
+        printf("\n%d",N[i]);
+    }
+}
